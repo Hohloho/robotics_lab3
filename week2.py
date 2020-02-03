0479cf0542262fd71c5480b189cb4734782bb1b8
Hohloho <krtcch121@gmail.com>
2020-02-03 15:25:53 +0700

    ffff

--- week2.py
+++ week2.py
@@ -1,7 +1,8 @@
 def validate_pin(pin):
     try:
         int(pin)
-        length = list(map(int, str(pin)))
+        if(pin>0):
+            length = list(map(int, str(pin)))
         if(len(length) == 4 or len(length) == 6):
             return True
         else:
