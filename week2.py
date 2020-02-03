ecb94c168bcbd834f0e40eabf33028b560228479
Hohloho <krtcch121@gmail.com>
2020-02-03 16:18:04 +0700

    thousandth commit

--- week2.py
+++ week2.py
@@ -1,13 +1,8 @@
 def validate_pin(pin):
-    try:
-        int(pin)
-        length = list(map(int, str(pin)))
-        if(pin<0):
-            return False
-        if(len(length) == 4 or len(length) == 6):
+    if (pin.isdigit()):
+        if(len(pin)==4 or len(pin)==6):
             return True
         else:
             return False
-    except ValueError:
+    else:
         return False
-
